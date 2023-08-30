ba99e994809d8fcb5699d60a9f5d46afd9a84af3
longdog <linda@longdog.us>
2023-08-30 16:29:16 +0400

    fix move from tmp (not work in docker)

--- deps.ts
+++ deps.ts
@@ -1,10 +1,9 @@
-import { type FormDataFile } from 'https://deno.land/x/oak@v12.5.0/mod.ts'
-import { join } from 'https://deno.land/std@0.190.0/path/mod.ts';
-import { ensureDirSync, moveSync } from 'https://deno.land/std@0.190.0/fs/mod.ts';
-
-export {
-  join,
+import { type FormDataFile } from "https://deno.land/x/oak@v12.5.0/mod.ts";
+import { join } from "https://deno.land/std@0.190.0/path/mod.ts";
+import {
   ensureDirSync,
   moveSync,
-  FormDataFile
-};
+  copySync,
+} from "https://deno.land/std@0.190.0/fs/mod.ts";
+
+export { join, ensureDirSync, moveSync, copySync, FormDataFile };
